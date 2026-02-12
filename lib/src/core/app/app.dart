3835ee4856aec0a20715de7c49edeafaacd29265
AJmal <96195451+Brahm03@users.noreply.github.com>
2026-02-12 16:44:36 +0500

    debug false qldm

--- lib/src/core/app/app.dart
+++ lib/src/core/app/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'PN app',
       onGenerateRoute: AppRouter.onGenerateRoute,
       initialRoute: '/login',
