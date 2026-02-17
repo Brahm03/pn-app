05d9fdc12101bec3316c4345ce28229210867efc
Shadowfeere <samuratovhasan2@gmail.com>
2026-02-17 16:36:35 +0500

    save the books screeni boshlandi

---
+++ lib/src/features/auth/presentation/screens/save_the_books.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SaveTheBooks extends StatelessWidget {
+  const SaveTheBooks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
