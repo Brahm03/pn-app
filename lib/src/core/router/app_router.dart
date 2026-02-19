7fa0e026eb9102dff18cca35bebd3e9f6873a31d
Пользователь <shohjahonqudratov794@gmail.com>
2026-02-19 15:05:01 +0500

    review ochildi

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pn_app/src/features/auth/presentation/screens/login_page.dart';
+import 'package:pn_app/src/features/review/presentation/screen/review_screen.dart';
 
 class AppRouter {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/login':
         return customRouter(LoginPage());
+          case '/review':
+        return customRouter(ReviewScreen());
       // case "sign_up":
       //   return customRouter();
       default:
